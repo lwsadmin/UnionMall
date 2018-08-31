31dea0f258a53245eeee382f79a4b82428554492
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2018-08-31 17:49:50 +0800

    1
    1

--- 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Controllers/MainController.cs
+++ 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Controllers/MainController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UnionMall.Controllers;
 
 namespace UnionMall.Web.Controllers
 {
+    [AbpMvcAuthorize]
     public class MainController : UnionMallControllerBase
     {
         public IActionResult Index()
