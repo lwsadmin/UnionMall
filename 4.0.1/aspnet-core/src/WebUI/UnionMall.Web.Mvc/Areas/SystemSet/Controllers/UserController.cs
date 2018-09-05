674e929945342addb72b79a4ec3f1985f957beb3
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2018-09-05 23:27:13 +0800

    0

--- 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/SystemSet/Controllers/UserController.cs
+++ 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/SystemSet/Controllers/UserController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Abp.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UnionMall.Controllers;
+using Castle.Core.Logging;
+using UnionMall.Users;
 
 namespace UnionMall.Web.Mvc.Areas.SystemSet.Controllers
 {
@@ -12,8 +14,17 @@ namespace UnionMall.Web.Mvc.Areas.SystemSet.Controllers
     [AbpMvcAuthorize]
     public class UserController : UnionMallControllerBase
     {
+        private readonly IUserAppService _userAppService;
+        public readonly ILogger _logger;
+        public UserController(IUserAppService userAppService, ILogger logger)
+        {
+            _userAppService = userAppService;
+            _logger = logger;
+        }
+
         public IActionResult List()
         {
+            Logger.Debug("-------------------");
             return View();
         }
     }
