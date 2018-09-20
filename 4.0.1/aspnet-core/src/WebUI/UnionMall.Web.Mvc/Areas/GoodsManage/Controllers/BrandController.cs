bc6fe7ace58addb7a0286ea3c3a5f3f2c8c730f1
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2018-09-20 17:58:45 +0800

    1
    1

--- 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/GoodsManage/Controllers/BrandController.cs
+++ 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/GoodsManage/Controllers/BrandController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UnionMall.Web.Mvc.Areas.GoodsManage.Controllers
 {
+    [AbpMvcAuthorize]
     public class BrandController : Controller
     {
         public IActionResult Index()
--- 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/GoodsManage/Controllers/CategoryController.cs
+++ 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/GoodsManage/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
 using Abp.Runtime.Session;
 using Microsoft.AspNetCore.Mvc;
 using UnionMall.Controllers;
@@ -12,6 +13,7 @@ using X.PagedList;
 namespace UnionMall.Web.Mvc.Areas.GoodsManage.Controllers
 {
     [Area("GoodsManage")]
+    [AbpMvcAuthorize]
     public class CategoryController : UnionMallControllerBase
     {
         private readonly IGoodsCategoryAppService _AppService;
@@ -23,7 +25,7 @@ namespace UnionMall.Web.Mvc.Areas.GoodsManage.Controllers
         }
         public IActionResult List(int page = 1)
         {
-            int pageSize = 15;
+            int pageSize = 10;
             string table = $"select *from tgoodscategory";
             if (_AbpSession.TenantId != null)
                 table += $" where TenantId={_AbpSession.TenantId}";
@@ -33,6 +35,19 @@ namespace UnionMall.Web.Mvc.Areas.GoodsManage.Controllers
             IPagedList pageList = new PagedList<DataRow>(ds.Tables[0].Select(), page, pageSize, total);
 
             return View(pageList);
+        }
+
+        public IActionResult Table(int page = 1,int pageSize= 10)
+        {
+            string table = $"select *from tgoodscategory";
+            if (_AbpSession.TenantId != null)
+                table += $" where TenantId={_AbpSession.TenantId}";
+
+            int total;
+            DataSet ds = _AppService.GetPage(page, pageSize, table, "id desc", out total);
+            IPagedList pageList = new PagedList<DataRow>(ds.Tables[0].Select(), page, pageSize, total);
+
+            return View("_Table", pageList);
         }
     }
 }
