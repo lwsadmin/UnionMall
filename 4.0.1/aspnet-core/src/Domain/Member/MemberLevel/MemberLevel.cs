58175e00bce9be04e591ed485ccac60e9c8dfa8e
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2018-11-08 19:47:48 +0800

    1

--- 4.0.1/aspnet-core/src/Domain/Member/MemberLevel/MemberLevel.cs
+++ 4.0.1/aspnet-core/src/Domain/Member/MemberLevel/MemberLevel.cs
@@ -3,17 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Abp.Domain.Entities;
-<<<<<<< HEAD
-using System.ComponentModel.DataAnnotations.Schema;
-namespace UnionMall.Entity
-{
-    [Table("TMemberLevel")]
-    public class MemberLevel : Entity<long>
-    {
-        public int? TenantId { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "title can not be null")]
-=======
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace UnionMall.Member.MemberLevel
 {
@@ -22,7 +14,7 @@ namespace UnionMall.Member.MemberLevel
         public int TenantId { get; set; }
 
         [Required(ErrorMessage = "title can not be null")]
->>>>>>> 9c9df8549ffd1b56db3183e0c18e537bafd0bf4c
+
         public string Title { get; set; }
 
         public decimal InitPoint { get; set; }
