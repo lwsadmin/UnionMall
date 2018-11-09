6ba6e33ee915051ca59558eb5738bad430a1c60a
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2018-11-09 17:47:37 +0800

    2
    2

---
+++ 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/Member/Controllers/MemberRegController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using UnionMall.Controllers;
+using UnionMall.Member;
+
+namespace UnionMall.Web.Mvc.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AbpMvcAuthorize("Member")]
+    public class MemberRegController : UnionMallControllerBase
+    {
+        private readonly IMemberLevelAppService _levelAppService;
+
+        public MemberRegController(IMemberLevelAppService levelAppService)
+        {
+            _levelAppService = levelAppService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var levelDropDown = (await _levelAppService.GetDropDown());
+            ViewData.Add("Level", new SelectList(levelDropDown, "Id", "Title"));
+            return View();
+        }
+    }
+}
