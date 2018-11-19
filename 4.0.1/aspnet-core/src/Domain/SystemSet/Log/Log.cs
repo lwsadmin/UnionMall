9e0a95fcef41cd5ec8f9a599c234fd4ff70b48d1
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2018-11-19 09:27:39 +0800

    1

---
+++ 4.0.1/aspnet-core/src/Domain/SystemSet/Log/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+
+namespace UnionMall.Entity
+{
+
+    [Table("TLog")]
+    public class Log : Entity<long>, IHasCreationTime, IMustHaveTenant
+    {
+        public int TenantId { get; set; }
+        public string Content { get; set; }
+        public string IPAddress { get; set; }
+        public string UserAccount { get; set; }
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+    }
+}   
