f1559034f7c7f944270d0014f2f32fc464ff43d7
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2018-12-16 21:21:54 +0800

    1
    1

---
+++ 4.0.1/aspnet-core/src/Application/SystemSet/WechatMessage/WechatMessageAppService.cs
@@ -0,0 +1,50 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Abp.Runtime.Session;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using UnionMall.IRepositorySql;
+
+namespace UnionMall.SystemSet
+{
+    public class WechatMessageAppService : ApplicationService, IWechatMessageAppService
+    {
+        private readonly ISqlExecuter _sqlExecuter;
+        public readonly IAbpSession _AbpSession;
+        private readonly IRepository<Entity.WechatMessage, long> _Repository;
+
+        public WechatMessageAppService(ISqlExecuter sqlExecuter,
+            IRepository<Entity.WechatMessage, long> Repository, IAbpSession AbpSession)
+        {
+            _sqlExecuter = sqlExecuter;
+            _Repository = Repository;
+            _AbpSession = AbpSession;
+        }
+        public DataSet GetPage(int pageIndex, int pageSize, string orderBy, out int total, string where = "", string table = "")
+        {
+            if (string.IsNullOrEmpty(table))
+            {
+                table = $@"select w.id,w.Title,w.Content,w.isopen  from TWechatMessage w where TenantId={AbpSession.TenantId}";
+            }
+            table += where;
+            return _sqlExecuter.GetPagedList(pageIndex, pageSize, table, orderBy, out total);
+        }
+
+        public async Task ChangeStatus(long id)
+        {
+            var query = await _Repository.FirstOrDefaultAsync(c => c.Id == id);
+            if (query != null)
+            {
+                if (query.IsOpen)
+                    query.IsOpen = false;
+                else
+                    query.IsOpen = true;
+
+                await _Repository.UpdateAsync(query);
+            }
+        }
+    }
+}
--- 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/WeChatSet/Controllers/MessageController.cs
+++ 4.0.1/aspnet-core/src/WebUI/UnionMall.Web.Mvc/Areas/WeChatSet/Controllers/MessageController.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using UnionMall.Controllers;
+using UnionMall.SystemSet;
+using X.PagedList;
 
 namespace UnionMall.Web.Mvc.Areas.WeChatSet.Controllers
 {
-    public class MessageController : Controller
+    [Area("WeChatSet")]
+    [AbpMvcAuthorize("WeChatSet.WeChatMessage")]
+    public class MessageController : UnionMallControllerBase
     {
-        public IActionResult List()
+        private readonly IWechatMessageAppService _appService;
+        public MessageController(IWechatMessageAppService appService)
         {
-            return View();
+            _appService = appService;
+        }
+
+        public async Task<IActionResult> List(string title,int page = 1, int pageSize = 10)
+        {
+            string where = string.Empty;
+            if (!string.IsNullOrEmpty(title))
+                where += $" and w.title like '%{title}%' ";
+            int total;
+            DataSet ds = _appService.GetPage(page, pageSize, "id desc", out total, where);
+            IPagedList pageList = new PagedList<DataRow>(ds.Tables[0].Select(), page, pageSize, total);
+            ViewBag.PageSize = pageSize;
+            if (Request.Headers.ContainsKey("x-requested-with"))
+            {
+                return View("_Table", pageList);
+            }
+            return View(pageList);
         }
     }
 }
