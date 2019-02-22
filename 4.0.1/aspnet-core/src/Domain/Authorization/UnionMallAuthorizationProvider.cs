da966ca63ca15f8d19197c7cd2b6405715cab6e1
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2019-02-22 17:59:34 +0800

    1
    1

--- 4.0.1/aspnet-core/src/Domain/Authorization/UnionMallAuthorizationProvider.cs
+++ 4.0.1/aspnet-core/src/Domain/Authorization/UnionMallAuthorizationProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 using System.Xml;
 using Abp.Authorization;
 using Abp.Localization;
@@ -16,10 +17,13 @@ namespace UnionMall.Authorization
             //context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"), multiTenancySides: MultiTenancySides.Host);
 
             XmlDocument NavigationXml = new XmlDocument();
-            string currentDirectory = Path.GetFullPath("../../Domain/Localization/XmlData/Navigation.xml");
+            //string currentDirectory = Path.GetFullPath("../../Domain/Localization/XmlData/Navigation.xml");
+            //string[] name = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            Stream sm = Assembly.GetExecutingAssembly().GetManifestResourceStream("UnionMall.Localization.XmlData.Navigation.xml");
+       
             XmlReaderSettings settings = new XmlReaderSettings();
             settings.IgnoreComments = true; //忽略注释
-            XmlReader reader = XmlReader.Create(currentDirectory, settings);
+            XmlReader reader = XmlReader.Create(sm, settings);
             NavigationXml.Load(reader);
             XmlNodeList List = NavigationXml.SelectNodes("//Navigation//First");
             foreach (XmlNode item in List)
